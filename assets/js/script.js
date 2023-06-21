e985df1cee1d35753bb7bc7018ac83d8203aea69
Marketing Director <nicolestark1103@gmail.com>
2023-06-21 09:57:36 -0500

    updated html and pseudocoded js

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// vars
+
+
+// vars to reference DOM elements
+
+
+// question arrays
+
+
+// initial state of quiz
+
+
+// function to start the game & hide intro
+
+
+// loop through question array & create list with buttons
+
+
+// check for right answer
+
+// deduct time for wrong answer
+
+// go to next question
+
+// function to end quiz
+    // stop timer
+    // show final schore
+
+// function to end quiz if timer runs out
+
+// save score and initials to localStorage 
+
+
